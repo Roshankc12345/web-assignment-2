a90732a262b00949654d5e6aa41c22bb8d48198a
Roshankc12345 <121548509+Roshankc12345@users.noreply.github.com>
2024-12-19 23:22:26 +0545

    Update get_users.php

--- get_users.php
+++ get_users.php
@@ -1,6 +1,6 @@
 <?php
-if (file_exists('users.txt')) {
-    $users = file('users.txt', FILE_IGNORE_NEW_LINES);
+if (file_exists('users.json')) {
+    $users = file('users.json', FILE_IGNORE_NEW_LINES);
     echo json_encode($users);
 } else {
     echo json_encode([]);
